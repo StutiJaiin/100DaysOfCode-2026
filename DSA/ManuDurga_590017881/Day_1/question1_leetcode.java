c242c90f5344e8109f36bd8b3bd33ef3bfbb1d71
ManuDurga <you@example.com>
2026-06-15 03:32:04 +0530

    Day 1 Questions

---
+++ DSA/ManuDurga_590017881/Day_1/question1_leetcode.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class question1_leetcode{
+    public static void main(String[] args){
+        int missingNumberSum = 0;
+        int totalNumberSum=0;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();          //this is the size of the input array
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();missingNumberSum=missingNumberSum+nums[i];}     //elements of that array
+        for(int j =0; j<=n;j++){totalNumberSum=totalNumberSum+(j+1);}
+        System.out.println(totalNumberSum-missingNumberSum);
+     }}
