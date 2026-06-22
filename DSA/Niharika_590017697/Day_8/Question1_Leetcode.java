6bda9df31b3649ea237811e166017639faf45ebc
Niharika Chand <nhrkchnd@gmail.com>
2026-06-22 23:40:30 +0530

    Day 8 solution

--- DSA/Niharika_590017697/Day_8/Question1_Leetcode.java
+++ DSA/Niharika_590017697/Day_8/Question1_Leetcode.java
@@ -5,15 +5,11 @@ class Solution {
         while (left < right) 
         {
             while (left < right && !Character.isLetterOrDigit(s.charAt(left))) 
-            {
                 left++;
-            }
 
             while (left < right && !Character.isLetterOrDigit(s.charAt(right))) 
-            {
                 right--;
-            }
-
+            
             if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) 
             {
                 return false;
